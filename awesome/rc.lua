7f00c26840998e41c3faf8afc628ce20514bbcd8
yurukute <nguyenkhanhdung.9999@gmail.com>
2021-10-02 04:08:28 +0700

    update

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -58,18 +58,18 @@ editor_cmd	= terminal .. " -e " .. editor
 browser		= "microsoft-edge-dev"
 file_manager	= "thunar"
 launcher	= "~/.config/rofi/launcher.sh"
-picture_dir	= "~/DATA/Pictures/"
+picture_dir	= "~/Pictures/"
 -- Default modkey.
 modkey		= "Mod4"
 altkey		= "Mod1"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
    awful.layout.suit.tile,
-   --awful.layout.suit.tile.left,
-   awful.layout.suit.fair,
-   awful.layout.suit.tile.bottom,
-   --awful.layout.suit.tile.top,
-   awful.layout.suit.fair.horizontal,
+   awful.layout.suit.tile.left,
+   awful.layout.suit.tile.bottom,   
+   awful.layout.suit.tile.top,
+   awful.layout.suit.fair,   
+   awful.layout.suit.fair.horizontal,   
    --awful.layout.suit.spiral,
    --awful.layout.suit.spiral.dwindle,
    --awful.layout.suit.max,
