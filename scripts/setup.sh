27a18f9670633976509bb916924eee5d380dafa2
yurukute <yurukute@gmail.com>
2023-09-28 23:13:17 +0700

    update kitty, setup script
    - kitty: modify keybindings
    - change wallpaper
    - script: fix typo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sudo bash
 bluetooth=(pulseaudio-bluetooth bluez bluez-utils)
-display=(feh picom rofi lxappearance arc-gtk-theme arc-icon-theme lightdm-ewbkit-theme-aether)
+display=(feh picom rofi lxappearance arc-gtk-theme arc-icon-theme lightdm-webkit-theme-aether)
 filemgr=(thunar tumbler gvfs)
 fonts=(ttf-dejavu ttf-droid ttf-liberation)
-media=(light alsa-utils pulseaudio-alsa pavucontrol flameshot)
-tool=(emacs numlockx ibus-bamboo)
-xorg=(xorg-server xogr-xinput xorg-xrandr xorg-font-util)
+media=(light alsa-utils pulseaudio-alsa pavucontrol)
+tools=(emacs numlockx ibus-bamboo flameshot)
+xorg=(xorg-font-util xorg-xmodmap xorg-server xorg-xinput xorg-xprop xorg-xrandr)
 
 yay_dir="$HOME/.cache/yay/yay"
 
@@ -19,9 +19,11 @@ if ! which yay &> /dev/null; then
     fi
 fi
 
-yay -S --needed ${font[@]} ${media[@]} ${display[@]} ${xorg[@]} ${filemgr[@]} ${tool[@]}
+yay -S --needed --noconfirm ${fonts[@]} ${media[@]} ${display[@]} ${xorg[@]} ${filemgr[@]} ${tools[@]}
 
 read -p "Do you want to install bluetooth packages? (y/n)" -n 1 -r
-ehco
+echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     yay -S --needed --noconfirm ${bluetooth[@]}
+    sudo systemctl enable bluetooth
+fi
